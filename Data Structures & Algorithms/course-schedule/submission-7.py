50ea500d84cacc34661566c55f12490ba0e2987e
Conor Fabian <110637682+conorfabian@users.noreply.github.com>
2026-09-25 15:59:07 -0700

    Add: course-schedule - submission-7

---
+++ Data Structures & Algorithms/course-schedule/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = {i: [] for i in range(numCourses)}
+
+        for course, pre in prerequisites:
+            graph[course].append(pre)
+
+        seen = set()
+        def dfs(node):
+            if node in seen:
+                return False
+
+            seen.add(node)
+            for nei in graph[node]:
+                if not dfs(nei):
+                    return False
+            seen.remove(node)
+            graph[node] = []
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+
+        return True
